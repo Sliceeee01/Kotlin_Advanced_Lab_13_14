c54b5c8e1870d627e8398abab1f78d305ba839c4
Sliceeee01 <ivan_hih@icloud.com>
2026-05-18 19:06:00 +0300

    sortedBy() function

--- src/HigherOrderFunctionsList.kt
+++ src/HigherOrderFunctionsList.kt
@@ -86,9 +86,16 @@ val cookies = listOf(
 //        println("${it.name} - $${it.price}")
 //    }
 //}
+//fun main() {
+//    val totalPrice = cookies.fold(0.0) { total, cookie ->
+//        total + cookie.price
+//    }
+//    println("Total price: $${totalPrice}")
+//}
 fun main() {
-    val totalPrice = cookies.fold(0.0) { total, cookie ->
-        total + cookie.price
+    val alphabeticalMenu = cookies.sortedBy { it.name }
+    println("Меню в алфавитном порядке:")
+    alphabeticalMenu.forEach {
+        println(it.name)
     }
-    println("Total price: $${totalPrice}")
 }
