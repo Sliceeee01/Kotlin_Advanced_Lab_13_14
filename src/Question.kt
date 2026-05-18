278341156a9082c6a8448651feb4d7bb566609f6
Sliceeee01 <ivan_hih@icloud.com>
2026-05-18 18:26:25 +0300

    Use scope-functions

--- src/Question.kt
+++ src/Question.kt
@@ -8,12 +8,7 @@ data class Question<T>(
     val difficulty: Difficulty
 )
 
-interface ProgressPrintable {
-    val progressText: String
-    fun printProgressBar()
-}
-
-class Quiz : ProgressPrintable {
+class Quiz {
     val question1 = Question<String>(
         questionText = "Речка спятила с ума – По домам пошла сама. ___",
         answer = "водопровод",
@@ -32,26 +27,24 @@ class Quiz : ProgressPrintable {
         difficulty = Difficulty.HARD
     )
 
-    companion object StudentProgress {
-        var total: Int = 0
-        var answered: Int = 0
-    }
-
-    override val progressText: String
-        get() = "${StudentProgress.answered} of ${StudentProgress.total} answered"
-
-    override fun printProgressBar() {
-        repeat(StudentProgress.answered) { print("█") }
-        repeat(StudentProgress.total - StudentProgress.answered) { print("░") }
-        println()
-        println(progressText)
+    fun printQuiz() {
+        question1.let {
+            println(it.questionText)
+            println(it.answer)
+        }
+        question2.let {
+            println(it.questionText)
+            println(it.answer)
+        }
+        question3.let {
+            println(it.questionText)
+            println(it.answer)
+        }
     }
 }
 
 fun main() {
-    Quiz.total = 10
-    Quiz.answered = 3
-
-    val quiz = Quiz()
-    quiz.printProgressBar()
+    Quiz().apply {
+        printQuiz()
+    }
 }
